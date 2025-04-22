0e8bc66d54b1ec94e73d7c55089f148bb645d381
Hal <hal.spidernight@gmail.com>
2025-04-22 23:54:59 +0900

    fix: fix project name

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,53 @@
 import { Plugin, UserConfig } from "vite";
 import { UserConfig as VPUserConfig } from "vitepress";
-import render from "./modules/render";
+// import render from "./modules/render";
 
 export type MCPPluginOptions = {
   port: number;
 };
 
+
+
 export function MCPPlugin(inlineOptions?: Partial<MCPPluginOptions>): Plugin {
   return {
-    name: "vitepress-plugin-mcp",
+    name: "vite-plugin-vitepress-mcp",
     enforce: "pre",
+    configureServer: (server) => {
+      console.log("start configureServer");
+    },
+    buildStart: (options) => {
+      console.log("start buildStart");
+    },
     config: (userConfig: UserConfig, env): VPUserConfig => {
-      const vpConfig = userConfig as VPUserConfig;
-        console.log("vpConfig", userConfig.plugins);
+      console.log("userConfig");
+      return userConfig;
+      // const vpConfig = userConfig as VPUserConfig;
+      //   console.log("vpConfig", userConfig.plugins);
 
-      const vtPlg = (userConfig.plugins ?? []).find((plg: any) => plg.name === "vitepress");
-      if (vtPlg && vtPlg.configResolved) {
-        const originalConfig = vtPlg.configResolved;
-        console.log("found vitepress");
-        vtPlg.configResolved = function (vpUserConfig: VPUserConfig) {
-          console.log("vpUserConfig", vpUserConfig);
-          const vpUserThemeConfig = (vpUserConfig as any).vitepress.userConfig.themeConfig;
+      // const vtPlg = (userConfig.plugins ?? []).find((plg: any) => plg.name === "vitepress");
+      // if (vtPlg && vtPlg.configResolved) {
+      //   const originalConfig = vtPlg.configResolved;
+      //   console.log("found vitepress");
+      //   vtPlg.configResolved = function (vpUserConfig: VPUserConfig) {
+      //     console.log("vpUserConfig", vpUserConfig);
+      //     const vpUserThemeConfig = (vpUserConfig as any).vitepress.userConfig.themeConfig;
 
-          console.log("vpUserThemeConfig", vpUserThemeConfig);
+      //     console.log("vpUserThemeConfig", vpUserThemeConfig);
 
-          if (!vpUserThemeConfig.themeConfig) vpUserThemeConfig.themeConfig = {};
+      //     if (!vpUserThemeConfig.themeConfig) vpUserThemeConfig.themeConfig = {};
 
-          if (!vpUserThemeConfig.themeConfig.search) vpUserThemeConfig.themeConfig.search = {};
+      //     if (!vpUserThemeConfig.themeConfig.search) vpUserThemeConfig.themeConfig.search = {};
 
-          if (!vpUserThemeConfig.themeConfig.search.options) vpUserThemeConfig.themeConfig.search.options = {};
+      //     if (!vpUserThemeConfig.themeConfig.search.options) vpUserThemeConfig.themeConfig.search.options = {};
 
-          // _renderを上書き
-          vpUserThemeConfig.themeConfig.search.options._render = async (src, env, md) => {
-            return await render(src, env, md);
-          };
-          return originalConfig.call(this, vpUserConfig);
-        };
-      }
-      return vpConfig;
+      //     // _renderを上書き
+      //     vpUserThemeConfig.themeConfig.search.options._render = async (src, env, md) => {
+      //       return await render(src, env, md);
+      //     };
+      //     return originalConfig.call(this, vpUserConfig);
+      //   };
+      // }
+      // return vpConfig;
     },
     // buildEnd: () => {
     //   // MCPサーバ起動
